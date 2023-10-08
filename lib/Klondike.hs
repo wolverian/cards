4fc12a9c99588d717b515d651b06765e6ee12abd
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2023-10-08 18:31:51 +0300

    happy

--- lib/Klondike.hs
+++ lib/Klondike.hs
@@ -1,28 +1,38 @@
 module Klondike where
 
-import Cards
-import System.Random (RandomGen (split), uniform, uniformR)
+import Cards (Card, Deck (..), shuffledDeck)
+import Data.Data (Proxy (Proxy))
+import Data.Maybe (fromJust)
+import Data.Vector qualified as Unsized
+import Data.Vector.Sized (Vector, enumFromN', fromSized, take', toList, toSized, unfoldrN)
+import Data.Vector.Sized qualified as Sized
+import GHC.TypeLits (KnownNat, SNat, type (+))
+import System.Random (RandomGen (split))
+import Prelude hiding (replicate)
 
-type Pile = [Card]
+type Pile = Unsized.Vector Card
 
-newtype Tableau = Tableau Pile
+newtype Tableau = Tableau (Vector 7 Pile)
     deriving (Show)
 
 newtype Stock = Stock Pile
     deriving (Show)
+    deriving newtype (Semigroup, Monoid)
 
 class FromDeck f where
     fromDeck :: Deck -> f
 
 instance FromDeck Stock where
     fromDeck :: Deck -> Stock
-    fromDeck (Deck cards) = Stock cards
+    fromDeck (Deck cards) = Stock (fromSized cards)
 
 newtype Waste = Waste Pile
     deriving (Show)
+    deriving newtype (Semigroup, Monoid)
 
-newtype Foundations = Foundations [Pile]
-    deriving (Show)
+newtype Foundations = Foundations (Vector 4 Pile)
+    deriving stock (Show)
+    deriving newtype (Semigroup, Monoid)
 
 data Game g = Game
     { tableau :: Tableau
@@ -38,14 +48,21 @@ initial g =
     let (g', g'') = split g
         deck = shuffledDeck g''
      in Game
-            { waste = Waste []
-            , foundations = Foundations [[], [], [], []]
+            { waste = Waste mempty
+            , foundations = mempty
             , stock = fromDeck (shuffledDeck g)
             , tableau = genTableau deck
             , randomGen = g'
             }
 
 genTableau :: Deck -> Tableau
-genTableau g =
-    let counts = [1 .. 7]
-     in (Tableau _, g)
+genTableau (Deck cards) =
+    let counts = [1 .. 7] :: [Int]
+        ([], columns) =
+            foldr
+                ( \n (cards', columns) ->
+                    let (column, rest) = splitAt n cards' in (rest, column : columns)
+                )
+                (toList cards, mempty)
+                counts
+     in Tableau (Unsized.fromList <$> fromJust (Sized.fromList columns))
