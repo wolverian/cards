90543d3cbdeeb0084a533f8949f50427870e27ac
Ilmari Vacklin <ilmari.vacklin@iki.fi>
2023-10-08 12:39:02 +0300

    add Cards

---
+++ lib/Cards.hs
@@ -0,0 +1,45 @@
+module Cards (
+    Suit (..),
+    Rank (..),
+    value,
+    Card (..),
+    Deck (..),
+    regularDeck,
+    Hand (..),
+) where
+
+data Suit = Diamond | Clubs | Hearts | Spades deriving (Read, Eq, Enum)
+
+instance Show Suit where
+    show :: Suit -> String
+    show = \case
+        Diamond -> "♦"
+        Clubs -> "♣"
+        Hearts -> "♥"
+        Spades -> "♠"
+
+data Rank = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King
+    deriving (Read, Eq, Ord, Enum)
+
+instance Show Rank where
+    show :: Rank -> String
+    show = show . value
+
+value :: Rank -> Int
+value = (+ 1) . fromEnum
+
+data Card = Card Suit Rank
+    deriving (Read, Eq)
+
+instance Show Card where
+    show :: Card -> String
+    show (Card suit rank) = show suit ++ " " ++ show rank
+
+newtype Deck = Deck [Card]
+    deriving (Show, Read, Eq)
+
+regularDeck :: Deck
+regularDeck = Deck [Card suit rank | suit <- [Diamond .. Spades], rank <- [Ace .. King]]
+
+newtype Hand = Hand [Card]
+    deriving (Show, Read, Eq)
