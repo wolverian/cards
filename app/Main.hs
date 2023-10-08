445a397617e3b896a647b191d667447de8ecc5f1
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2023-10-08 18:55:55 +0300

    exe

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,14 @@ module Main where
 
 import Cards qualified
 import Klondike qualified
+import System.Random qualified as Random
 
 data Game = Game
     deriving (Show)
 
 main :: IO ()
-main = print Cards.regularDeck
+main = do
+    print Cards.regularDeck
+    g <- Random.initStdGen
+    print $ Cards.shuffledDeck g
+    print $ Klondike.initial g
